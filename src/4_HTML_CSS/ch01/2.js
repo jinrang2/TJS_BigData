b5ff36d3594c5665e6c3d378e4d5474e0848bf61
jinrang2 <jinrang2@naver.com>
2021-01-13 18:04:34 +0900

    210113

---
+++ src/4_HTML_CSS/ch01/2.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+/*여러줄 주석
+ * */
+
+// 한줄 주석
+name = prompt("이름이 뭐니?", "홍길동");
+document.write(name + "~ Welcome <br>");
+function chk() {
+    if (frm.tel.value.length < 4) {
+        alert("전화번호는 뒷자리 4자이상 입력해야해");
+        return false;
+    }
+}
